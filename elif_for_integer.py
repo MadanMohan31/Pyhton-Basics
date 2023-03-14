105eda004f179eacec44e718b01b1e09bccb4884
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-03-14 21:46:31 +0530

    Update elif_for_integer.py

--- elif_for_integer.py
+++ elif_for_integer.py
@@ -13,6 +13,11 @@ def max_num(num1,num2,num3):
 
 print(max_num(200, 500, 50))
 
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+c = int(input("enter the third number : "))
+print(max_num(a, b, c))
+
 # > greater than
 # < less than
 # == equal to
