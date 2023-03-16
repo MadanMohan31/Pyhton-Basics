53bf0b8fc8a79f25ab7cbbc24e01a58e90b346a6
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-03-16 22:38:52 +0530

    Create for.py

---
+++ for.py
@@ -0,0 +1,22 @@
+# code to execute for loop
+
+
+for letter in "Virat Kohli":
+    print(letter)  # print every letter of the string
+    
+    
+print("")
+friends = ["Modi", "King", "Queen"]
+for friend in friends:
+    print(friend)
+    
+for index in range(len(friends)):
+    print(friends[index])
+    
+    
+print("")
+for index in range(10):
+    print(index)
+    
+for index in range(3, 10):
+    print(index)
