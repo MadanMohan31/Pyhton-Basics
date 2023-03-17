b6a060d93cdb856c30d8f3d49d6dcc78264ea178
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-03-17 22:33:07 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,10 @@
+# a single line comment.
+print("Executed after single line comment.")
+print("")
+
+'''
+3 ' is used for multiple line comments.
+this will comment multiple lines.
+a paragraph type comment can be done.
+'''
+print("Executed after multiple line comment.")
