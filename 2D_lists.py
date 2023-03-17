18acb40a46397fe90a93464ca4efe5c614d50b9a
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-03-17 22:31:57 +0530

    Create 2D_lists.py

---
+++ 2D_lists.py
@@ -0,0 +1,16 @@
+# code to represent 2-Dimensional Lists.
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]    
+]
+
+# First [] addresses index of list.
+print(number_grid[1])
+print(number_grid[3])
+
+# Second [] addresses inner lists value.
+print(number_grid[1][0])
+print(number_grid[2][2])
