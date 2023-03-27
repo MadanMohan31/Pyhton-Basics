55f60019f3642b74380c4476fb739c81435cbb61
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-03-27 19:31:42 +0530

    Create pip.py

---
+++ pip.py
@@ -0,0 +1,13 @@
+# usage of pip
+
+"""
+1. open the command prompt
+2. check the version by clicking 
+   pip --version
+   u will get the version.
+3. install python-docx by using
+   pip install python-docx
+4. now u can check the installed doc in going to the LIB folder in python directories.
+5. inside LIB we get SITE-PACKAGES
+6. inside SITE-PACKAGES we get the python_docx-0.8.6-py3.6.egg-info
+"""
