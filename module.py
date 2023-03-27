a7bf2a62b7d5a1d2e4afd0460de4c055aee25f29
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-03-27 19:31:10 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,14 @@
+# code to demonstrate modules
+
+'''
+here i will use return1.py file to use that cube function in my code.
+'''
+
+import return1
+
+print(return1.cube(3))
+
+'''
+NOTE : for more inbuilt modules, browse 'LIST OF PYTHON MODULES' 
+       and look into the website which is applicable with the current version of the python file, which you are using
+'''
