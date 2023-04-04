0e73ee69f333a8d13c294c13e5cf5f0172c01f22
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-04 21:11:48 +0530

    Create inheritance_chef.py

---
+++ inheritance_chef.py
@@ -0,0 +1,13 @@
+# this is the basic class created to use chef class.
+
+class chef:
+    
+    def salad(self):
+        print("chef makes salad.")
+    
+    def chicken(self):
+        print("chef makes chicken varieties.")
+        
+    def special_dish(self):
+        print("chef makes KFC chicken as his special dish.")
+        
