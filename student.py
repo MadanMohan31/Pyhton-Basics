89b85ecfddf8d1b9b6d1d9c65449369bc1de9256
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-04 21:03:26 +0530

    Update student.py

--- student.py
+++ student.py
@@ -4,6 +4,8 @@ in this code we build the student class and define the variables in which the va
 class can be used anytime in other codes by using import function.
 '''
 
+
+# here we create only one function inside the class.
 class student:
     
     def __init__(self, name, major, gpa, is_on_probation):
@@ -12,3 +14,17 @@ class student:
         self.gpa = gpa
         self.is_on_probation = is_on_probation
         
+    
+# here we create two or more class in the same class.
+class member:
+    
+    def __init__(self, name, major, gpa):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        
+    def good_student(self):
+        if self.gpa >= 7.5:
+            return True
+        else:
+            return False
