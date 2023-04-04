0fa99800e4008d71b18b8dcc44ee2761c30e36f0
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-04 21:12:29 +0530

    Create inheritance_chinese_chef.py

---
+++ inheritance_chinese_chef.py
@@ -0,0 +1,11 @@
+# here we import the class chef to import completely into the chinesecook class.
+
+from inheritance_chef import chef
+
+class chinesecook(chef):
+    
+    def special_dish(self):
+        print("chinese chef makes lemon chicken as his special dish.")
+        
+    def fried_rice(self):
+        print("chinese chef makes fried rice.")
