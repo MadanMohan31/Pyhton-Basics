29f791a6c2e054ea9cf9fb6040db1f4624cfeb31
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-04 21:11:07 +0530

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+# this code is to represent the properties of inheritance in pyhton.
+
+# here we import class cook from inheritance_chef.
+from inheritance_chef import chef
+
+# here we import class chinesecook from inheritance_chinese_chef.
+from inheritance_chinese_chef import chinesecook
+
+mychef = chef()
+mychef.special_dish()
+
+mychinesechef = chinesecook()
+mychinesechef.special_dish()
