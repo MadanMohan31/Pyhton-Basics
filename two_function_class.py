4aa8935e541567778fe92b162a97ed644be92cfd
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-04 21:01:03 +0530

    Create two_function_class.py

---
+++ two_function_class.py
@@ -0,0 +1,8 @@
+'''
+question class is created to read the question and answer.
+'''
+
+class question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
